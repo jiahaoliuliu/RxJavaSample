e5f01625b3d1e67b625bd1abbec21643c6259389
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2017-06-26 23:16:23 +0400

    Added RxJava2. Created a simple obsevable

--- app/src/main/java/com/jiahaoliuliu/rxjavasample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/rxjavasample/MainActivity.java
@@ -3,11 +3,37 @@ package com.jiahaoliuliu.rxjavasample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Observable<Integer> integerObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createObservable();
+    }
+
+    private void createObservable() {
+        integerObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+
+                // Once the observable has emitted all the items in the sequence,
+                // call on Complete
+                e.onComplete();
+            }
+        });
     }
 }
