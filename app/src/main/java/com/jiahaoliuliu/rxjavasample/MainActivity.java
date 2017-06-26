31477bb8025be44f74950dfb86c9fa2b7c0b2be0
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2017-06-26 23:27:02 +0400

    Creating integer observable

--- app/src/main/java/com/jiahaoliuliu/rxjavasample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/rxjavasample/MainActivity.java
@@ -2,6 +2,7 @@ package com.jiahaoliuliu.rxjavasample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -11,6 +12,8 @@ import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "RxJavaExample";
+
     private Observable<Integer> integerObservable;
 
     @Override
@@ -18,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createObservable();
+        createIntObservable();
+        createIntObserver();
+
     }
 
-    private void createObservable() {
+    private void createIntObservable() {
         integerObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
@@ -35,5 +40,32 @@ public class MainActivity extends AppCompatActivity {
                 e.onComplete();
             }
         });
+    }
+
+    private void createIntObserver() {
+        Observer<Integer> myObserver = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.v(TAG, "OnSubscribe: " + d);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.v(TAG, "OnNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.v(TAG, "OnError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.v(TAG, "OnComplete: ");
+            }
+        };
+
+        // The observable subscribe to the observer
+        integerObservable.subscribe(myObserver);
     }
 }
